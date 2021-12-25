031b3378831ef95a29c3c074fc168872b2154edc
KhalidMansoor0310 <khalidmansoor725@gmail.com>
2021-12-25 02:33:22 -0800

    react starter

---
+++ src/Lecture1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+let name = "khalid mansoor"
+export default function Lecture1() {
+    return (
+        <div>
+            <h1>My name is {name}</h1>
+        </div>
+    )
+}
