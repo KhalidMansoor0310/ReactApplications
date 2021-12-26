5aaf22ef8cc16e0c2ad78eac7ecac4055b2e75a5
KhalidMansoor0310 <khalidmansoor725@gmail.com>
2021-12-26 01:38:00 -0800

    enabling mode in application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 // import Cards from './components/Cards'
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 
 
 export default function App() {
+    const [mode,setMode] = useState('dark');
+    const toggleMode=()=>{
+        if(mode==='dark'){
+            setMode('light');
+            document.body.style.backgroundColor='white';
+        }
+        else{
+            setMode('dark');
+            document.body.style.backgroundColor='darkblue';
+
+        }
+    }
     return (
         <>
-            <Navbar title='khalid First React Application'/>
-            <TextForm/>
+            <Navbar title='khalid First React Application' mode={mode} toggleMode={toggleMode}/>
+            <TextForm mode={mode}/>
         </>
         
     )
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,17 +1,19 @@
 import React,{useState} from 'react'
 
-function TextForm() {
+function TextForm(props) {
 
     const [text,setText] = useState('Enter some text here....');
-
+    
     const HandleUpClick=()=>{
         let newText = text.toUpperCase();
         setText(newText);
     }
+    
     const HandleLowerClick=()=>{
         let newText = text.toLowerCase();
         setText(newText);
     }
+  
     const HandleRemoveClick=()=>{
         let newText = '';
         setText(newText);
@@ -22,22 +24,24 @@ function TextForm() {
     return (
         <>
         
-        <div className='container my-3'>
+        <div className='container my-3' style={{color:props.mode==='dark'?'white':'black'}}>
             <h1>Enter Text Below...</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={HandleOnchange} id="floatingTextarea" rows='8'></textarea>  
                 <button className="btn btn-primary mt-3 mx-3" onClick={HandleUpClick}>Click To UpperCase</button> 
                 <button className="btn btn-primary mt-3 mx-3" onClick={HandleLowerClick}>Click To lowerCase</button> 
                 <button className="btn btn-danger mt-3 mx-3" onClick={HandleRemoveClick}>Click To RemoveAll</button> 
+                
+
             </div>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
             <h2>Summary of your Work...</h2>
             <p>{text.split(' ').length} Words and {text.length} charachters</p>
             <h3>Time to read</h3>
             <p>{0.008*text.split(' ').length}</p>
             <h3>Preview</h3>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter some text above in the text field to preview it Here....."}</p>
         </div>
         </>
     )
